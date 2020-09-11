69666d60f96d1dc34aeaa4308d8cd44a95a3411b
Milosh Videnov <milos.videnov@gmail.com>
2020-09-11 18:22:20 +0300

    Weather App - first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
 
 function App() {
+  const API_KEY = "27246259aabc1b11ce70b1b84bb09bf4";
+  const [input, setInput] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const getWeather = (e) => {
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input}&units=metric&appid=${API_KEY}`)
+        .then((res) => res.json())
+        .then((result) => {
+          setInput("");
+          setWeather(result);
+          console.log(result);
+        })
+        .catch(err=>
+          alert("Please enter correct city name!")
+        );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <main>
+        <div className="search-container">
+          <h1 className="heading">Weather Application</h1>
+          <p className="text1">Enter a city below in the box for search: </p>
+          <input className="search-bar" type="text" placeholder="Search..."
+            onChange={(e) => setInput(e.target.value)} value={input}
+          />
+           <button className="submit" type="submit" onClick={getWeather} >Submit</button>
+        </div>
+        {typeof weather.main != "undefined" ? (
+          <div className="data-container">
+            <div className="location"> City: {weather.name}, {weather.sys.country}</div>
+            <div className="temperature"> Temperature: {weather.main.temp}°C</div>
+            <div className="weather"> Weather: {weather.weather[0].main}</div>
+          </div>
+        ) : (
+          ""
+        )}
+      </main> 
   );
 }
 
